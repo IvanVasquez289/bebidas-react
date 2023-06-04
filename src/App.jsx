1d7fa0021861acff67f940b6abd93ab3f058cf3a
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-06-03 18:33:51 -0600

    Creando el context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Container } from "react-bootstrap";
 import Formulario from "./components/Formulario";
+import { CategoriasProvider } from "./context/CategoriasProvider";
 function App() {
   return (
-    <>
+    <CategoriasProvider>
       <header className='py-5'>
         <h1>Buscador de bebidas</h1>
       </header>
@@ -10,7 +11,7 @@ function App() {
       <Container className="mt-5">
         <Formulario/>
       </Container>
-    </>
+    </CategoriasProvider>
   );
 }
 
---
+++ src/context/CategoriasProvider.jsx
@@ -0,0 +1,21 @@
+import { createContext , useState, useEffect} from "react";
+
+const CategoriasContext = createContext();
+
+const CategoriasProvider = ({children}) => {
+    return(
+        <CategoriasContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </CategoriasContext.Provider>
+    )
+}
+
+export {
+    CategoriasProvider
+}
+
+export default CategoriasContext
