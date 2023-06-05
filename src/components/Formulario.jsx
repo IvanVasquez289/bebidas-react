e2e90889cdf287ad402312eb27e4a6d0f8f49b2d
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-06-05 01:06:01 -0600

    Obteniendo las categorias de la API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,11 @@
 import { Form,Button,Row,Col} from "react-bootstrap"
+import useCategorias from "../hooks/useCategorias"
+
 const Formulario = () => {
+  const {categorias} = useCategorias()
   return (
     <Form>
-        <Row>
+        <Row className="mb-3">
             <Col md={6}>
                 <Form.Group>
                     <Form.Label htmlFor="bebida"> Nombre bebida:</Form.Label>
@@ -22,8 +25,24 @@ const Formulario = () => {
                         name="cateoria"
                     >
                         <option value="">--Selecciona Categoria</option>
+                        {categorias.map(categoria => (
+                            <option 
+                                value={categoria.strCategory}
+                                key={categoria.strCategory}
+                            >
+                                {categoria.strCategory}
+                            </option>
+                        ))}
                     </Form.Select>
                 </Form.Group>
+            </Col>
+        </Row>
+
+        <Row className="justify-content-end">
+            <Col md={3}>
+                <Button variant="danger" className="text-uppercase w-100">
+                    Buscar bebidas
+                </Button>
             </Col>
         </Row>
     </Form>
