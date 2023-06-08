a6d9f8e6dd613c4488272f51c4077abc05646dc4
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-06-07 23:23:19 -0600

    Agregando el componente Card a las bebidas

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { Col, Card, Button } from 'react-bootstrap'
 const Bebida = ({bebida}) => {
+  console.log(bebida)
   return (
-    <div>{bebida.strDrink}</div>
+    <Col md={6} lg={3} className='mb-3 mt-4'>
+        <Card>
+            <Card.Img variant="top" src={bebida.strDrinkThumb} />
+            <Card.Body>
+                <Card.Title>{bebida.strDrink}</Card.Title>
+                {/* <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+                </Card.Text> */}
+                <Button variant="warning" className='w-100 text-uppercase mt-2 '>Ver receta</Button>
+            </Card.Body>
+        </Card>
+    </Col>
   )
 }
 
